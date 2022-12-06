77c3e6dca10936c0395e7878ef0db7358e855014
samivytech <119914135+samivytech@users.noreply.github.com>
2022-12-05 23:36:32 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+""""
+Author: Sam Kirsch
+Class: Intro to Software Development
+Last Modified: 12/5/22
+
+This is a program that allows the user to select a major or minor guitar chord chart
+"""
+
+# import tkinter
+
+import tkinter as tk
+
+class GUI:
+    # create main window
+    def __init__(self):
+        self.homeWindow = tk.Tk()
+
+# major chords window
+    def __init__(self):
+        self.majorWindow = tk.Tk()
+
+# minor chords window
+g = GUI()
